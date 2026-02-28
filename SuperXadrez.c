7ab2ecd7463d39c16e27284cfaa2fc5f9df95c94
andrealvesxp <andrealvesxp@gmail.com>
2026-02-28 19:51:44 -0300

    Update SuperXadrez.c
    Atualizacao do codigo

--- SuperXadrez.c
+++ SuperXadrez.c
@@ -1,37 +1,305 @@
 #include <stdio.h>
- 
-int main(){
-    //variaveis do contador e da identidade
-    int i;
-    int contador = 0;
- 
-    //movimento da torre
-    printf("Torre :\n");
-    for (i = 0; i <= 5; i++) 
+
+void exibirMenu()
+{
+    printf("    QUAL PEÇA DESEJA MOVER   \n");
+    printf("  1 - BISPO \n");
+    printf("  2 - RAINHA \n");
+    printf("  3 - TORRE \n");
+    printf("  4 - CAVALO \n");
+    printf("  5 - SAIR DO JOGO\n");
+}
+
+void moverRainha(int rainha)
+{
+    if (rainha > 0)
     {
-        printf("Direita\n");
+        printf("  SUBIU \n");
+        moverRainha(rainha - 1);
     }
- 
-    //movimento do bispo
-    printf("Bispo :\n");
-    while (contador < 5) 
+}
+
+void moverBispo()
+{
+    int opcao_bispo;
+    int opcao_bispo2;
+    printf("  1 - DIAGONAL DIREITA\n  2 - DIAGONAL ESQUERDA\n");
+    printf(" Escolha uma opção: ");
+    scanf("%d", &opcao_bispo);
+    switch (opcao_bispo)
     {
-        printf("Cima\n");
-        printf("Direita\n");
-        contador = contador + 1;
+    case 1:
+        printf("==================================================\n");
+        printf("  1 - SUPERIOR\n  2 - INFERIOR\n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &opcao_bispo2);
+        switch (opcao_bispo2)
+        {
+        case 1:
+            for (int bispo = 1; bispo <= 5; bispo++)
+            {
+                for (int bispo = 1; bispo <= 1; bispo++)
+                {
+                    printf("  SUBIU -");
+                }
+                printf(" DIREITA \n");
+            }
+            break;
+        case 2:
+            for (int bispo = 1; bispo <= 5; bispo++)
+            {
+                for (int bispo = 1; bispo <= 1; bispo++)
+                {
+                    printf("  DESCEU -");
+                }
+                printf(" DIREITA \n");
+            }
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+        break;
+    case 2:
+        printf("  1 - SUPERIOR\n  2 - INFERIOR\n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &opcao_bispo2);
+        switch (opcao_bispo2)
+        {
+        case 1:
+            for (int bispo = 1; bispo <= 5; bispo++)
+            {
+                for (int bispo = 1; bispo <= 1; bispo++)
+                {
+                    printf("  SUBIU -");
+                }
+                printf(" ESQUERDA \n");
+            }
+            break;
+        case 2:
+            for (int bispo = 1; bispo <= 5; bispo++)
+            {
+                for (int bispo = 1; bispo <= 1; bispo++)
+                {
+                    printf("  DESCEU -");
+                }
+                printf(" ESQUERDA \n");
+            }
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+        break;
+    default:
+        printf("OPÇÃO INVÁLIDA!!!\n");
+        break;
     }
- 
-    contador = 0; 
- 
-    //movimento da rainha
-    printf("Rainha :\n");
-    do 
+}
+
+void moverTorre(int torre)
+{
+    do
     {
-        printf("Esquerda\n");
-        contador = contador + 1;
-      
-    } 
-      while (contador < 8);
- 
+        printf("  DIREITA \n");
+        torre++;
+    } while (torre <= 4); // Laço de repetição, ele vai parar quando a Torre se "mover" 5 casas.
+}
+
+void moverCavalo()
+{
+    int mover_horizontal = 1;
+    int mover_vertical = 1;
+    int mover_vertical2;
+    printf("  1 - PARA CIMA\n  2 - PARA BAIXO\n  3 - ESQUERDA\n  4 - DIREITA \n");
+    printf(" Escolha uma opção: ");
+    scanf("%d", &mover_vertical);
+
+    switch (mover_vertical)
+    {
+    case 1: // MOVIMENTAÇÃO DO CAVALO PARA CIMA!!!!
+        printf("  1 - ESQUERDA\n  2 - DIREITA \n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &mover_horizontal);
+        switch (mover_horizontal)
+        {
+        case 1: // APOS IR PARA CIMA, ELE VAI FAZER A MOVIMENTAÇÃO PARA A ESQUERDA!!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  SUBIU \n");
+                }
+                printf("  ESQUERDA \n");
+            }
+            break;
+        case 2: // APOS IR PARA CIMA, ELE VAI FAZER A MOVIMENTAÇÃO PARA A DIREITA!!!
+            while (mover_vertical <= 1)
+            {
+                while (mover_horizontal <= 3)
+                {
+                    printf("  SUBIU \n");
+                    mover_horizontal++;
+                }
+                printf("  DIREITA \n");
+                mover_vertical++;
+            }
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+        break;
+    case 2: // MOVIMENTAÇÃO DO CAVALO PARA BAIXO!!!!
+        printf("  1 - ESQUERDA\n  2 - DIREITA \n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &mover_horizontal);
+        switch (mover_horizontal)
+        {
+        case 1: // APÓS IR PARA BAIXO, ELE VAI FAZER MOVIMENTAÇÃO PARA ESQUERDA!!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  DESCEU \n");
+                }
+                printf("  ESQUERDA \n");
+            }
+            break;
+        case 2: // APÓS IR PARA BAIXO, ELE VAI FAZER MOVIMENTAÇÃO PARA DIREITA!!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  DESCEU \n");
+                }
+                printf("  DIREITA \n");
+            }
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+
+        break;
+    case 3: // MOVIMENTAÇÃO DO CAVALO PARA ESQUERDA !!!
+        printf("  1 - PARA CIMA\n  2 - PARA BAIXO \n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &mover_vertical2);
+        switch (mover_vertical2)
+        {
+        case 1: // APÓS IR PARA ESQUERDA, ELE VAI FAZER MOVIMENTAÇÃO PARA CIMA !!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  ESQUERDA \n");
+                }
+                printf("  CIMA \n");
+            }
+
+            break;
+        case 2: // APÓS IR PARA ESQUERDA, ELE VAI FAZER MOVIMENTAÇÃO PARA BAIXO !!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  ESQUERDA \n");
+                }
+                printf("  DESCEU \n");
+            }
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+        break;
+    case 4: // MOVIMENTAÇÃO DO CAVALO PARA DIREITA !!!
+        printf("  1 - PARA CIMA\n  2 - PARA BAIXO \n");
+        printf(" Escolha uma opção: ");
+        scanf("%d", &mover_vertical2);
+        switch (mover_vertical2)
+        {
+        case 1: // APÓS IR PARA DIREITA, ELE VAI FAZER MOVIMENTAÇÃO PARA CIMA !!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  DIREITA \n");
+                }
+                printf("  CIMA \n");
+            }
+            break;
+        case 2: // APÓS IR PARA DIREITA, ELE VAI FAZER MOVIMENTAÇÃO PARA BAIXO !!!
+            for (mover_vertical = 1; mover_vertical <= 1; mover_vertical++)
+            {
+                for (mover_horizontal = 1; mover_horizontal <= 2; mover_horizontal++)
+                {
+                    printf("  DIREITA \n");
+                }
+                printf("  DESCEU \n");
+            }
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+        break;
+    }
+}
+
+void Sair()
+{
+    printf("     Até Mais...  \n");
+}
+
+int main()
+{
+
+    int escolher_peca;
+
+    do
+    {
+        // Menu
+        exibirMenu(0);
+        scanf("%d", &escolher_peca);
+
+        // Criando um switch case para o Menu interativo
+        switch (escolher_peca)
+        {
+        case 1:
+            printf("           BISPO        \n");
+
+            moverBispo(0); // A função bispo, vai subir o BISPO em 5 casas diagonal.
+
+            break;
+        case 2:
+            printf("           RAINHA         \n");
+
+            moverRainha(6); // A função rainha, vai subir a RAINHA em 5 casas.
+
+            break;
+        case 3:
+            printf("           TORRE         \n");
+
+            moverTorre(0); // A função torre, vai mover a TORRE em 5 casas a direita.
+
+            break;
+        case 4:
+            printf("           CAVALO         \n");
+
+            moverCavalo(0); // A função cavalo, irá subir o CAVALO em formato de L.
+
+            break;
+
+        case 5: // Quando o usuario selecionar 5, vai sair do laço de repetição e terminar o programa
+            Sair();
+            break;
+
+        default:
+            printf("OPÇÃO INVÁLIDA!!!\n");
+            break;
+        }
+    } while (escolher_peca != 5);
+
     return 0;
 }
